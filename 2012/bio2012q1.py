912b0c3c81b122a6d17e42a79805a44897459783
kari-coffee <80966924+kari-coffee@users.noreply.github.com>
2022-11-26 21:02:38 +0000

    fixed bug where 2 returned 1

--- 2012/bio2012q1.py
+++ 2012/bio2012q1.py
@@ -6,10 +6,10 @@ res = 1
 
 def is_prime(n):
     # simplest and most efficient algorithm for finding primes I found
-    if n <= 1 or n % 2 == 0 or n % 3 == 0:
-        return False
-    elif n == 2 or n == 3:
+    if n == 2 or n == 3:
         return True
+    elif n <= 1 or n % 2 == 0 or n % 3 == 0:
+        return False
     
     for i in range(5, int(sqrt(n))+1, 6):
         if n % i == 0 or n % (i+2) == 0:
