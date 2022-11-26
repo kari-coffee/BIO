e820f251e364abe16d9f128515fcd8425df0c9ef
kari-coffee <80966924+kari-coffee@users.noreply.github.com>
2022-11-26 20:54:33 +0000

    much improved version

--- in progress/bio2012q1.py
+++ in progress/bio2012q1.py
@@ -5,6 +5,7 @@ res = 1
 
 
 def is_prime(n):
+    # simplest and most efficient algorithm for finding primes I found
     if n <= 1 or n % 2 == 0 or n % 3 == 0:
         return False
     elif n == 2 or n == 3:
@@ -18,15 +19,36 @@ def is_prime(n):
 if is_prime(n):
     print(n)
 
+    # V1
+# else:
+#     #find all primes up to n (maybe don't need to check all the way up to n?)
+#     primes = [2, 3]
+#     for i in range(5, n, 2):
+#         if is_prime(i):
+#             primes.append(i)
+#     #print(primes)
+#     #check which primes are factors
+#     for i in primes:
+#         if n % i == 0:
+#             res *= i
+#             #print(i)
+            
+#     print(res)
+
+    # V2 - checks next prime only when needed - much faster!
 else:
-    primes = [2, 3]
-    for i in range(5, n, 2):
+    done = False
+    prime = 2
+    for i in range(3, n, 2):
+        if n == 1:
+            break
+        while n % prime == 0:
+            n //= prime
+            if not done:
+                res *= prime
+            done = True
         if is_prime(i):
-            primes.append(i)
-    #print(primes)
-    for i in primes:
-        if n % i == 0:
-            res *= i
-            #print(i)
+            prime = i
+            done = False
             
     print(res)
